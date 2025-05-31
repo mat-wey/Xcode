4cad1b050aa30f7750bc3fc52af85cdbb60de283
Practicum <practicum@ya.ru>
2025-05-31 20:48:06 +0300

    initialized git

---
+++ guess the number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guess the number
+//
+//  Created by Матвей Симонов on 31.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
