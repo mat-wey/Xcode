27552bc1f0287328b0757e30a1a20fd1b88ac956
Practicum <practicum@ya.ru>
2025-05-31 21:27:20 +0300

    push dvelod

--- guess the number/ViewController.swift
+++ guess the number/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var number: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = ""
     }
-
+    var RandomNamber = Int.random(in: 1...10)
+    @IBAction func угадай(_ sender: Any) {
+        guard Int(number.text!) != nil else { return }
+        if Int(number.text!) == RandomNamber {
+            label.text = "ВЫ ВЫИГРАЛИ!!!!!!!!"
+        }else{
+            label.text = "*/звук свечка/"
+        }
+    }
 
 }
 
